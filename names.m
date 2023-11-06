bc49f68d49fe266781540a6c723f199d80922385
WaqiMTC <20B2068@ubd.edu.bn>
2023-11-07 05:50:36 +0800

    Removed temp files
    Removed files (integrated into plotWord.m:
    - endpoint.m
    - splitter.m
    - translator.m
    
    Updated files:
    - plotWord.m (now a real function)"
    
    Added files:
    - names.m

---
+++ names.m
@@ -0,0 +1,2 @@
+string_input = 'Waqi // Ros // Fizah // Wana';
+plotWord(string_input)
--- plotWord.m
+++ plotWord.m
@@ -1,33 +1,57 @@
-% Define data points for x and y coordinates
-% x = [0 0.5 1 1.5 2 2.5 3.5 2.5 1.5 2.5 3 3.5 4 4.5 5];
-% y = [1 2   1 2   1 2   3   4   3   2   1 2   1 2   1];
-% x = [1.0 2.0 3.0 4.0 3.0 2.0 2.0 2.0 2.0 2.0 2.0 2.0 3.0 4.0]
-% y = [3.5 3.5 3.5 3.5 3.5 3.5 2.5 1.0 2.5 2.5 2.5 2.5 2.5 2.5]
-% x = [1.50 0.50 0.00 0.50 1.50 1.75 1.50 1.75 2.50];
-% y = [1.75 1.50 0.50 0.00 0.25 0.75 1.75 0.75 0.00];
-x = [0.40 0.70 0.80 1.00 0.80 0.80 1.00 0.90 0.50 0.90 1.40];
-y = [0.30 0.40 0.50 0.90 0.50 0.50 0.30 0.10 0.00 0.10 0.35];
-
+function plotWord(string_input)
+% clears current figure
 clf
 
-n = length(x);
-t = 0:n-1; % Parametric coordinate t
-tt = 0:0.01:n-1; % More dense coordinate tt for spline interpolation
+% breaks string_input around // 
+% then count how many lines the string has been cut down into
+breakdown = strip(split(string_input, "//"));
+lines = length(breakdown);
 
-% Compute spline interpolation
-xx = spline(t, x, tt);
-yy = spline(t, y, tt);
+% Iterate through lines
+for i = 1:length(breakdown)
+    % Opens breakdown to get the string 
+    string_line = breakdown(i);
+    string_line = string_line{1};
 
-% Plot settings
-figure(1)
-plot(xx, yy, 'k', 'LineWidth', 1.5) % plot spline
-hold on
-plot(x, y, 'bo', 'MarkerFaceColor','b') % plot data points
-axis([-0.2 6 -0.2 4.5])
-grid on
-title('Spline Interpolation of Cursive Letter D')
-xlabel('x')
-ylabel('y')
-% p = 0.5;
-% comet(xx, yy, p)
-grid on
+    % Iterate through characters
+    end_point = 0;
+
+    for j = 1:length(string_line)
+        % If there is space, leave a space about the size of one small
+        % letter
+        if string_line(j) == ' '
+            end_point = end_point + 2;
+
+        % else, plot the letters
+        else
+            [x y] = letters_database(string_line(j));
+            x = x + end_point;       % Shifting letters to the right
+            y = y + 6 * (lines - 1); % Shifting letters down based on line no.
+
+            n = length(x);
+            t = 0:n-1; % Parametric coordinate t
+            tt = 0:0.01:n-1; % More dense coordinate tt for spline interpolation
+
+            % Compute spline interpolation
+            xx = spline(t, x, tt);
+            yy = spline(t, y, tt);
+
+            % Plot settings
+            figure(1)
+            plot(xx, yy, 'k', 'LineWidth', 1.5) % plot spline
+            hold on
+            plot(x, y, 'bo', 'MarkerFaceColor','b') % plot data points
+            % axis([-0.2 6 -0.2 4.5])
+            grid on
+
+            % Recheck if the furthest x-point on that letter is 
+            % bigger than current endpoint
+            if max(x) > end_point
+                end_point = max(x);
+            end
+        end
+    end
+
+    % Shift letters down every line
+    lines = lines - 1;
+end
