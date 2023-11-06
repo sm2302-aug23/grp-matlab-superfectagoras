e652b9104583a12aa36f06c8cc40e1e4d8560376
WaqiMTC <20B2068@ubd.edu.bn>
2023-11-07 07:56:40 +0800

    Update plotWord.m
    Updated so the axes will adjust according to length and height of string in plot

--- plotWord.m
+++ plotWord.m
@@ -7,6 +7,12 @@ clf
 breakdown = strip(split(string_input, "//"));
 lines = length(breakdown);
 
+% check min and max x and y points for axis
+x_min = 999;
+x_max = -999;
+y_min = 999;
+y_max = -999;
+
 % Iterate through lines
 for i = 1:length(breakdown)
     % Opens breakdown to get the string 
@@ -28,6 +34,23 @@ for i = 1:length(breakdown)
             x = x + end_point;       % Shifting letters to the right
             y = y + 8 * (lines - 1); % Shifting letters down based on line no.
 
+            % compare with max and min
+            if max(x) > x_max
+                x_max = max(x);
+            end
+
+            if min(x) < x_min
+                x_min = min(x);
+            end
+
+            if max(y) > y_max
+                y_max = max(y);
+            end
+
+            if min(y) < y_min
+                y_min = min(y);
+            end
+
             n = length(x);
             t = 0:n-1; % Parametric coordinate t
             tt = 0:0.01:n-1; % More dense coordinate tt for spline interpolation
@@ -41,6 +64,7 @@ for i = 1:length(breakdown)
             plot(xx, yy, 'k', 'LineWidth', 1.5) % plot spline
             hold on
             plot(x, y, 'bo', 'MarkerFaceColor','b') % plot data points
+            axis([x_min x_max y_min y_max])
             % axis([-0.2 6 -0.2 4.5])
             grid on
 
